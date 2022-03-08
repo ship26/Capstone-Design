e8c5a5ffe87d9dc4c423b280b66bcdb217aac314
ship26 <86402582+ship26@users.noreply.github.com>
2022-03-08 22:13:09 +0900

    first

---
+++ Assets/Scene2Scripts/cubestatus.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class cubestatus : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public Transform target;
+    public Transform start;
+    NavMeshAgent agent;
+
+    public RaycastHit hit;
+
+   
+
+    void Start()
+    {
+
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        start = GameObject.FindWithTag("Enemy").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+
+
+        if (Physics.Raycast(start.position, start.forward, out hit, 10))
+        {
+            if (hit.collider.CompareTag("bush"))
+            {
+                agent.isStopped = true;
+            }
+            if (hit.collider.CompareTag("ground"))
+            {
+                agent.isStopped = false;
+            }
+
+        }
+
+
+
+    }
+
+
+  
+
+   
+
+}
---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private PlayerInput playerinput;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CameraTurn();
+
+        transform.Rotate(0, -90, 0);
+
+      
+
+
+
+
+    }
+
+
+    private void CameraTurn()
+    {
+
+        if(player.transform.eulerAngles.y >=270 && player.transform.eulerAngles.y<=360)
+        {
+            transform.position = player.transform.position + new Vector3(-player.transform.eulerAngles.y / 400, 6.6f, -400 / player.transform.eulerAngles.y);  //modify this code (not correct )_
+        }
+
+
+    }
+
+
+ 
+}
